65336b55fbad6379c9d264e8e82f593c5cdc7439
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-23 13:50:44 +0900

    django.shortcuts render 사용

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
 
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/indext.html", context)
 
 
 def detail(request, question_id):
