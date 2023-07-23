ff7736fa8a2682ec496488864d985635af656064
Keonwoo Moon <swervingturtle@gmail.com>
2023-07-23 15:10:56 +0900

    generic view 사용

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     # ex) polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     # ex) polls/1/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/", views.DetailView.as_view(), name="detail"),
     # ex) polls/1/results
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/results/", views.ResultsView.as_view(), name="results"),
     # ex) polls/1/vote
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
